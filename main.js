b3c85e146e291078534372030b144e0885c78842
sharad joshi <sharadjoshi87@gmail.com>
2020-04-19 21:05:33 +0530

    panchang added in homepage

--- main.js
+++ main.js
@@ -31,7 +31,78 @@ function deferVideo() {
 
 }
 window.onload = deferVideo;
+var currentDate = new Date();
 
-// $(document).ready(function(){
-//     $("#VidModal").modal('show');
-// });
+var date = currentDate.getDate();
+var month = currentDate.getMonth(); //Be careful! January is 0 not 1
+var year = currentDate.getFullYear();
+var dateString = '';
+
+if (month <= 9){
+    if (date <= 9){
+        dateString = '0'+ date + "-" +'0'+ (month + 1) + "-" + year;   
+    }
+    dateString = date + "-" +'0'+ (month + 1) + "-" + year;
+}else{
+    if(date <= 9){
+    dateString = '0'+ date + "-" +(month + 1) + "-" + year;
+    }
+    dateString = date + "-" +(month + 1) + "-" + year;
+}
+
+let today = document.getElementById('date');
+let sunRise = document.getElementById('sunr');
+let sunSet = document.getElementById('suns');
+let moonRise = document.getElementById('moonr');
+let moonSet = document.getElementById('moons');
+let Tithi = document.getElementById('tithi');
+let Naksh = document.getElementById('naksh');
+let Yog = document.getElementById('yog');
+let karan = document.getElementById('Karan');
+let varr = document.getElementById('Var');
+
+var sunr = '';
+var suns = '';
+var moonr = '';
+var moons = '';
+var tithi = '';
+var naksh = '';
+var yog = '';
+var Karan = '';
+var Var = '';
+
+$(document).ready(function(e){
+    // $("#punchang").modal('show');
+    // $('#sunrise').html('sunrise');
+    // e.preventDefault();
+    var mymodal = $('#punchang');
+    // mymodal.find('.modal-body').text('hello');
+    setPunchangData();
+    mymodal.modal('show');
+});
+
+//०५:५८ ए. एम. ०६:४४ पी. एम.
+function setPunchangData() {
+    today.innerHTML = '<b>पञ्चाङ्गम्</b>' + '&nbsp;||&nbsp;<b>' + dateString + '</b>';
+    if (dateString == '20-04-2020'){
+        sunr = '|| ०५:४९ ए. एम.';
+        suns = '|| ०६:४९ पी. एम.';
+        moonr = '|| ०५:०३ ए. एम., अप्रैल २१';
+        moons = '|| ०४:३१ पी. एम.';
+        tithi = '|| कृष्ण पक्ष त्रयोदशी - ०३:११ ए. एम., अप्रैल २१ तक ||चतुर्दशी';
+        naksh = '|| पूर्व भाद्रपद - ०७:२३  ए. एम. तक || उत्तर भाद्रपद';
+        yog = '|| इन्द्र - ०८:३२ पी. एम. तक || वैधृति';
+        Karan = '|| गर - ०१:५७ पी. एम. तक || वणिज - ०३:११ ए एम, अप्रैल २१ तक || विष्टि';
+        Var = 'सोमवार';
+    }
+    sunRise.innerHTML = sunr;
+    sunSet.innerHTML = suns;
+    moonRise.innerHTML = moonr;
+    moonSet.innerHTML = moons;
+    Tithi.innerHTML = tithi;
+    Naksh.innerHTML = naksh;
+    Yog.innerHTML = yog;
+    karan.innerHTML = Karan;
+    varr.innerHTML = Var;
+    
+}
